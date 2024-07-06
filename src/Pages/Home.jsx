319ffe4500d2b300ac31345fc6e759dcffe38832
abelsileshie <sileshieabel@gmail.com>
2024-07-06 12:36:28 +0300

    Updated spacing between photo and the text on the index page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,46 +1,12 @@
 import { Button, Typography } from "@material-tailwind/react";
 import React from "react";
 import Homepic from "/images/Home.jpg";
+import Index from "../Components/Index";
 
 const Home = () => {
   return (
     <div className="bg-homeBg">
-      <div className="container mx-auto p-4 lg:p-20">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-          <div className={`mt-8 lg:mt-0 lg:pt-32 animate-slide-in`}>
-            {" "}
-            {/* Apply the animation class here */}
-            <Typography variant="h6" className="text-xs">
-              Hello, I am Abel Sileshie
-            </Typography>
-            <Typography className="text-2xl lg:text-4xl font-extrabold mt-4">
-              I am a Front End{" "}
-              <span className="text-[#5E3BEE]">Website Developer</span> And
-              UI/UX Designer
-            </Typography>
-            <Typography className="text-sm lg:text-md font-body text-justify text-[#282938] mt-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit.
-            </Typography>
-            <div className="flex space-x-4 mt-8">
-              <Button className="w-24 bg-[#5E3BEE] rounded-none">Email</Button>
-              <Button className="w-24 bg-[#5E3BEE] rounded-none">
-                Contact
-              </Button>
-            </div>
-          </div>
-          <div className="mt-8 lg:mt-0 lg:p-24 rounded-2xl w-full h-full flex justify-center lg:justify-end">
-            <img
-              src={Homepic}
-              alt="Home"
-              className="rounded-3xl w-full lg:w-auto lg:max-w-md"
-            />
-          </div>
-        </div>
-      </div>
+      <Index />
     </div>
   );
 };
